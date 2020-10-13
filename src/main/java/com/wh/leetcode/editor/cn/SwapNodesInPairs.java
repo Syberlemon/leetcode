7ec2a123a5e36e0d2e4d9b9dbbd1e712627de78e
wanghao1 <wanghao1@hualala.com>
2020-10-13 14:16:33 +0800

    24. 两两交换链表中的节点

---
+++ src/main/java/com/wh/leetcode/editor/cn/SwapNodesInPairs.java
@@ -0,0 +1,60 @@
+package com.wh.leetcode.editor.cn;
+//给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。 
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 
+//
+// 示例: 
+//
+// 给定 1->2->3->4, 你应该返回 2->1->4->3.
+// 
+// Related Topics 链表 
+// 👍 678 👎 0
+
+public class SwapNodesInPairs {
+    public static void main(String[] args) {
+        Solution solution = new SwapNodesInPairs().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    //递归 ，一次操作交换挨着的两个元素， 交换完后的next 是下一个递归，递归结束为没有节点或一个节点 这种情况不需要交换
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = newHead.next;
+        newHead.next = head;
+        head.next = swapPairs(head.next);
+        return newHead;
+    }
+
+    public ListNode swapPairs_answer(ListNode head){
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null){
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
