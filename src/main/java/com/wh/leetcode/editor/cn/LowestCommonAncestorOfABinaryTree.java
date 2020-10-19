ca93bda83681bee6ece0e9984152f6452848cb19
wanghao1 <wanghao1@hualala.com>
2020-10-19 17:39:35 +0800

    236. 二叉树的最近公共祖先 由于无序，从根开始找路径不太好找，从叶子找根是唯一的，从根开始找叶子有太多条路需要排除

--- src/main/java/com/wh/leetcode/editor/cn/LowestCommonAncestorOfABinaryTree.java
+++ src/main/java/com/wh/leetcode/editor/cn/LowestCommonAncestorOfABinaryTree.java
@@ -36,7 +36,11 @@ package com.wh.leetcode.editor.cn;
 // 👍 763 👎 0
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class LowestCommonAncestorOfABinaryTree {
     public static void main(String[] args) {
@@ -75,21 +79,72 @@ class Solution {
         return ancestor;
     }
 
-    //给一个节点，在二叉树找到其路径 ???? 不会写 找到对的path后不知道怎么停止迭代
+    //给一个节点，在二叉树找到其路径, 先序就行，需要递归；
+    //递归结束条件：节点匹配到了，路径走到头了；但是递归每次要做的内容不清楚；
     private void getPath(TreeNode node, TreeNode p, List<TreeNode> path){
-//        path.add(node);
-//        if(node == p){
-//            return;
-//        }
-//        if(node.left != null){
-//            getPath(node.left, p, path);
-//        }
-//        if(node.right != null){
-//            getPath(node.right, p, path);
-//        }
-//        if(node.left == null && node.right == null){
-//            return ;
-//        }
+        if(node == p){
+            path.add(node);
+            return;
+        }
+        if(node.left != null){
+            path.add(node);
+            getPath(node.left, p, path);
+        }
+        if(node.right != null){
+            path.add(node);
+            getPath(node.right, p, path);
+        }
+        if(node.left == null && node.right == null){
+            //说明这条路径走到头了，还是没找到目标节点，即上一个节点走错了
+            //这里是用subList更好，还是remove更好
+            path = path.subList(0, path.size()-1);
+        }
+    }
+    private TreeNode ans = null;
+    private boolean dfs(TreeNode node, TreeNode p, TreeNode q){
+        if(node  == null){
+            return false;
+        }
+        boolean lson = dfs(node.left, p, q);
+        boolean rson = dfs(node.right, p, q);
+        //若两个节点，一个在左子树，一个在右子树，则当前节点为最近公共祖先；
+        //若两个节点，一个是当前节点，一个是当前节点的孩子，则当前节点也是最近公共祖先
+        if((lson && rson) || (node.val == p.val || node.val == q.val)&&(lson || rson)){
+            ans = node;
+        }
+        return lson || rson || (node.val == p.val || node.val == q.val);
+    }
+    public TreeNode lowestCommonAncestor_answer(TreeNode root, TreeNode p, TreeNode q){
+        this.dfs(root, p, q);
+        return this.ans;
+    }
+
+    //将每个节点的父亲存下来，从p走到根，然后从q往根走时判断是否访问过。因为都是从叶子往根走，故先匹配到的即离q最近的，即目标
+    Map<Integer, TreeNode> parent = new HashMap<>();
+    Set<Integer> visited = new HashSet<>();
+    private void dfs(TreeNode node){
+        if(node.left != null){
+            parent.put(node.left.val, node);
+            dfs(node.left);
+        }
+        if(node.right != null){
+            parent.put(node.right.val, node);
+            dfs(node.right);
+        }
+    }
+    public TreeNode lowestCommonAncestor_answer2(TreeNode root, TreeNode p, TreeNode q){
+        dfs(root);
+        while(p != null){
+            visited.add(p.val);
+            p = parent.get(p.val);
+        }
+        while (q != null){
+            if(visited.contains(q.val)){
+                return q;
+            }
+            q = parent.get(q.val);
+        }
+        return null;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
